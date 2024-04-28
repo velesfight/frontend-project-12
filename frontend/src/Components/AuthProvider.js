fde059e9b91fa5ffee028e70dc6803849543fa6b
Ekaterina Vinnikova <red-white.katya@mail.ru>
2024-04-28 19:02:27 -0400

    soc

--- frontend/src/Components/AuthProvider.js
+++ frontend/src/Components/AuthProvider.js
@@ -4,13 +4,23 @@ import AuthContext from '../Components/contexts/index.jsx';
 const AuthProvider = ({ children }) => {
 const [loggedIn, setLoggedIn] = useState(false);
 
-const logIn = () => setLoggedIn(true);
-  const logOut = () => {
-    localStorage.removeItem('userId');
-    setLoggedIn(false);
+const logIn = (userData) => {
+  localStorage.setItem('userId', JSON.stringify(userData));
+  setLoggedIn(true);
+};
+const logOut = () => {
+  localStorage.removeItem('userId');
+  setLoggedIn(false);
+};
+
+const getToken = () => {
+  if (loggedIn) {
+    return user.token;
   }
+  return null;
+};
   return (
-    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut, getToken }}>
       {children}
     </AuthContext.Provider>
   );
