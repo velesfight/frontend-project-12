e62d1cfa460f9b636f3f337544f31d2a6014d52f
Ekaterina Vinnikova <red-white.katya@mail.ru>
2024-08-18 18:14:05 -0400

    jfjjf

--- frontend/src/Components/SignUpPage/SignUp.jsx
+++ frontend/src/Components/SignUpPage/SignUp.jsx
@@ -135,12 +135,13 @@ const SignUp = () => {
                 }
                 required
               />
+              <Form.Label htmlFor="passwordConfirmation">{t('logIn.confirmPass')}</Form.Label>
               <Form.Control.Feedback type="invalid" tooltip>
               {authFailed
               ? t('validation.409')  
               : t(formik.touched.passwordConfirmation && formik.errors.passwordConfirmation)}
                 </Form.Control.Feedback>
-                <Form.Label htmlFor="passwordConfirmation">{t('logIn.confirmPass')}</Form.Label>
+                
                
             </Form.Group>
             <Button type="submit" className="w-100 mb-3 btn btn-outline-primary">{t('logIn.doRegister')}</Button>
