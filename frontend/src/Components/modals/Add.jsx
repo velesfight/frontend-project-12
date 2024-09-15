38c6a05108cbea86d47111c4054b62a0f1775eda
Ekaterina Vinnikova <red-white.katya@mail.ru>
2024-09-15 10:21:17 -0400

    jjj

--- frontend/src/Components/modals/Add.jsx
+++ frontend/src/Components/modals/Add.jsx
@@ -7,7 +7,7 @@ import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { hideModal } from '../../slices/uiSlisec'
 import { Modal, Form, Button } from 'react-bootstrap';
-import { selectors } from '../../slices/apiSlece';
+import { selectors, setCurrentChannelId } from '../../slices/apiSlece';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { useAuth } from '../../contexts/useAuth';
@@ -46,7 +46,7 @@ useEffect(() => {
       try {
         const response = await axios.post(routes.channelsPath(), newChannel,  { headers:  { Authorization: `Bearer ${getAuthToken()}` }});
         dispatch(addChannel(response.data));
-      //  dispatch(setCurrentChannelId(response.data.id));
+      dispatch(setCurrentChannelId(response.data.id));
         dispatch(hideModal());
 
         toast.success(t('modals.doneChannel'));
