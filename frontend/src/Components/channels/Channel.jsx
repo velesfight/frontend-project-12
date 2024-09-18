b6b110672a7028cc9aada1d06f1b1ec339d21918
Ekaterina Vinnikova <red-white.katya@mail.ru>
2024-09-18 07:37:10 -0400

    las

--- frontend/src/Components/channels/Channel.jsx
+++ frontend/src/Components/channels/Channel.jsx
@@ -6,60 +6,59 @@ import { Dropdown, Button, ButtonGroup, Nav } from 'react-bootstrap';
 import React from 'react';
 
 
-const Channel = ({ channel }) => {
-  const { t } = useTranslation();
-  const currentChannelId = useSelector(
+const Channel = ({channel}) => {
+ const { t } = useTranslation();
+ const currentChannelId = useSelector(
     (state) => state.channels.currentChannelId,
   );
-  const dispatch = useDispatch();
+ const variant = channel.id === currentChannelId ? 'secondary' : null;
+ const dispatch = useDispatch();
 
-  const changeChannel = (id) => {
-    dispatch(setCurrentChannelId(id));
-  };
-
-  // Назначение стилей для кнопок
-  const buttonClass = `w-100 rounded-0 text-start text-truncate btn ${channel.id === currentChannelId ? 'btn-secondary' : 'btn-primary'}`;
-  const dropdownToggleClass = `flex-grow-0 ${channel.id === currentChannelId ? 'btn-secondary' : 'btn-primary'}`;
-
-  return (
-    <Nav.Item key={channel.id} className="nav-item w-100">
-      {channel.removable ? (
-        <div role="group" className="d-flex dropdown btn-group">
-          <Button
-            type="button"
-            className={buttonClass}
-            onClick={() => changeChannel(channel.id)}
-          >
-            <span className="me-1">#</span>
-            {channel.name}
-          </Button>
-          <Dropdown as={ButtonGroup} className="d-flex">
-            {/* Используем переменную для стилей, чтобы исключить конфликт классов */}
-            <Dropdown.Toggle split className={dropdownToggleClass}>
-              <span className="visually-hidden">{t('chatPage.menu')}</span>
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={() => dispatch(showModal({ modalType: 'removing', channelId: channel.id }))}>
-                {t('modals.removeChannel')}
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => dispatch(showModal({ modalType: 'renaming', channelId: channel.id }))}>
-                {t('modals.renameChannel')}
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      ) : (
-        <Button
-          type="button"
-          className={buttonClass}
-          onClick={() => changeChannel(channel.id)}
-        >
-          <span className="me-1">#</span>
-          {channel.name}
-        </Button>
-      )}
-    </Nav.Item>
-  );
+const changeChannel = (id) => {
+dispatch(setCurrentChannelId(id));
 };
 
+
+return (
+            <Nav.Item key={channel.id} className="nav-item w-100">
+              {channel.removable ? (
+                <div role="group" className="d-flex dropdown btn-group">
+                              <Button
+                                  type="button"
+                                  className="w-100 rounded-0 text-start text-truncate"
+                                  onClick={() => changeChannel(channel.id)}
+                                  variant={variant}
+                              >
+                                  <span className="me-1">#</span>
+                                  {channel.name}
+                              </Button>
+                              <Dropdown as={ButtonGroup} className="d-flex">
+                              <Dropdown.Toggle split variant={variant} className="flex-grow-0">
+                              <span className="visually-hidden">{t('chatPage.menu')}</span>
+                              </Dropdown.Toggle>
+                              <Dropdown.Menu>
+                                  <Dropdown.Item onClick={() => dispatch(showModal({ modalType: 'removing', channelId: channel.id }))}>
+                                      {t('modals.removeChannel')}
+                                  </Dropdown.Item>
+                                  <Dropdown.Item onClick={() => dispatch(showModal({ modalType: 'renaming', channelId: channel.id }))}>
+                                      {t('modals.renameChannel')}
+                                  </Dropdown.Item>
+                              </Dropdown.Menu>
+                          </Dropdown>
+                          </div>
+                  ) : (
+                          <Button
+                              type="button"
+                              className="w-100 rounded-0 text-start"
+                              key={channel.id}
+                              onClick={() => changeChannel(channel.id)}
+                              variant={variant}
+                          >
+                              <span className="me-1">#</span>
+                              {channel.name}
+                          </Button>
+                  )}
+ </Nav.Item>
+)};
+            
 export default Channel;
