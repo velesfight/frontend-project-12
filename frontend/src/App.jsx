794891f9c55a6990ba528f41ed077b1841a6bc1d
Ekaterina Vinnikova <red-white.katya@mail.ru>
2024-10-07 16:26:22 -0400

    jjj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate,
-  useLocation } from 'react-router-dom';
-import  { useAuth }  from './contexts/useAuth'
+import { 
+  BrowserRouter, Routes, Route, Navigate,
+  useLocation 
+} from 'react-router-dom';
+import { useAuth } from './contexts/useAuth';
 import MainPage from './Components/MainPage.jsx';
 import LoginPage from './Components/LoginPage/LoginPage';
 import NotFoundPage from './Components/NotFoundPage.jsx';
-import AuthProvider from './Components/AuthProvider.js'
-import SignUp from './Components//SignUpPage/SignUp';
+import AuthProvider from './Components/AuthProvider.js';
+import SignUp from './Components/SignUpPage/SignUp';
 import { Provider } from 'react-redux';
-import store from './Components/store.js'
+import store from './Components/store.js';
 import routes from './Components/routes/routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -21,7 +23,7 @@ const PrivateRoute = ({ children }) => {
 
   return (
     user ? children : (
-    <Navigate to={routes.loginPath()} state={{ from: location }} />
+      <Navigate to={routes.loginPath()} state={{ from: location }} />
     )
   );
 };
@@ -29,39 +31,41 @@ const PrivateRoute = ({ children }) => {
 const App = () => {
   return (
     <div className="h-100" id="chat">
-    <Provider store={store}>
-    <AuthProvider>
-    <BrowserRouter>
-    <div className="d-flex flex-column h-100">
-    <HeaderChat />
-      <Routes>
-      <Route
-            path={routes.chatPage()}
-            element={(
-              <PrivateRoute>
-                <MainPage />
-              </PrivateRoute>
-            )}
-          />
-          <Route
-          path={routes.loginPath()}
-          element={<LoginPage />}
-          />
-          <Route 
-          path={routes.signUpPath()} 
-          element={<SignUp />} 
-          />
-        <Route
-        path="*" element={<NotFoundPage />}
-        />
-        </Routes>
-        <ToastContainer position='top-center' />
-        </div>
-    </BrowserRouter>
-    </AuthProvider>
-    </Provider>
+      <Provider store={store}>
+        <AuthProvider>
+          <BrowserRouter>
+            <div className="d-flex flex-column h-100">
+              <HeaderChat />
+              <Routes>
+                <Route
+                  path={routes.chatPage()}
+                  element={(
+                    <PrivateRoute>
+                      <MainPage />
+                    </PrivateRoute>
+                  )}
+                />
+                <Route
+                  path={routes.loginPath()}
+                  element={<LoginPage />}
+                />
+                <Route 
+                  path={routes.signUpPath()} 
+                  element={<SignUp />} 
+                />
+                <Route
+                  path="*"
+                  element={<NotFoundPage />}
+                />
+              </Routes>
+              <ToastContainer position="top-center" />
+            </div>
+          </BrowserRouter>
+        </AuthProvider>
+      </Provider>
     </div>
   );
 }
 
 export default App;
+
